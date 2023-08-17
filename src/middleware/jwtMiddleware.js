d4f9a420c1e21da2ec15130bba409a901facf9f2
Winifred <46898257+winigalviz@users.noreply.github.com>
2023-08-17 13:41:13 -0500

    config

---
+++ src/middleware/jwtMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports.authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+        console.log(err);
+        if (err) return res.sendStatus(403);
+        console.log(user);
+        next();
+    });
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const jwtMiddleware = require('../middleware/jwtMiddleware');
+
 const personajeRouter = require('./PersonajeRouter');
 const peliculaSerieRouter = require('./PeliculaSerieRouter');
 const autenticacionRouter = require('./AutenticacionRouter');
 
 /* GET home page. */
-router.use('/characters', personajeRouter);
-router.use('/movies', peliculaSerieRouter);
+router.use('/characters', jwtMiddleware.authenticateToken, personajeRouter);
+router.use('/movies', jwtMiddleware.authenticateToken, peliculaSerieRouter);
 router.use('/auth', autenticacionRouter);
 
 module.exports = router;
